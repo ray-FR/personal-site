ebff5756a53b6f40aedcaa3ebb96f3e40d66b793
R.I <ares1345istaken@gmail.com>
2026-02-04 12:46:14 +0100

    removed broken stuff

--- app.js
+++ app.js
@@ -23,15 +23,6 @@ button.addEventListener('click', function() {
 
 
 
-const menu = document.getElementById("menu");
-
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
-
   
 
 language.addEventListener('change', () => {
